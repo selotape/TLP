c8f1107ac2f18387970af19021fdb623efa033a6
Ron Visbord <ronvisbord@gmail.com>
2017-10-20 22:08:48 +0300

    add signup POST method

--- TLP/app.py
+++ TLP/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,20 @@ def hello_world():
 @app.route('/showSignUp')
 def show_sign_up():
     return render_template('signup.html')
+
+
+@app.route('/signUp', methods=['POST'])
+def sign_up():
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html': '<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html': '<span>Enter the required fields</span>'})
 
 
 if __name__ == '__main__':
