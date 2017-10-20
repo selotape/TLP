668fabc10aea90f9f1d5ab940a5ebc3d82c43767
Ron Visbord <ronvisbord@gmail.com>
2017-10-20 22:16:00 +0300

    prepare for password hashing

--- TLP/app.py
+++ TLP/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, json, request
+from werkzeug.security import generate_password_hash  # , check_password_hash
 
 app = Flask(__name__)
 
@@ -19,9 +20,10 @@ def sign_up():
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
+    _hashed_password = generate_password_hash(_password)
 
     # validate the received values
-    if _name and _email and _password:
+    if _name and _email and _hashed_password:
         return json.dumps({'html': '<span>All fields good !!</span>'})
     else:
         return json.dumps({'html': '<span>Enter the required fields</span>'})
