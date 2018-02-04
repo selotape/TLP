b91bff48ab72d776ff60267fa667c611e3fd85e7
Ron <ronvisbord@gmail.com>
2018-02-05 00:12:47 +0200

    initial implementation of user store

--- TLP/test/users/test_user_store.py
+++ TLP/test/users/test_user_store.py
@@ -1,13 +1,16 @@
 import string
 
+from TLP.users import UserStore
 
-def test_party_partitioning(user_store):
+
+def test_party_partitioning(user_store: UserStore):
     users = list(string.ascii_lowercase)
     for user in users:
-        user_store.put(user)
+        user_store.put_user(user, user)
 
     party_size = 4
     parties = list(user_store.get_parties(size=party_size))
-    assert len(parties) == string.ascii_lowercase // party_size
-    assert all(len(party) == party_size for party in parties[:-1])
-    assert len(parties[-1] == party_size + len(users) % party_size)
+    assert len(parties) == len(string.ascii_lowercase) // party_size
+    len_parties = [len(party) for party in parties]
+    assert sum(len_parties) == len(users)
+    assert all(party_size <= len_ <= party_size + 1 for len_ in len_parties)
--- TLP/users/user_store.py
+++ TLP/users/user_store.py
@@ -1,6 +1,50 @@
-class UserStore:
-    def put(self, user):
-        pass
+from boltons.iterutils import chunked
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session, Query
 
-    def get_parties(self):
-        pass
+_Base = declarative_base()
+
+
+class UserStore:
+    def __init__(self):
+        engine = create_engine('sqlite:///:memory:')
+        self._sessionmaker = sessionmaker(bind=engine, autocommit=True)
+        self._session: Session = self._sessionmaker()
+        _Base.metadata.create_all(engine)
+
+    def put_user(self, name, email):
+
+        user = User(name=name, email=email, _active=True)
+        self._session.add(user)
+
+    def get_parties(self, size):
+        # query: Query = session.query(User).filter_by(_active=True)
+        query: Query = self._session.query(User)
+        active_users = query.all()
+        for user in active_users:
+            user._active = False
+        self._session.add_all(active_users)
+
+        return self._chunk(active_users, size)
+
+    @staticmethod
+    def _chunk(active_users, size):
+        extra_count = len(active_users) % size
+        extra_users = active_users[-extra_count:]
+        chunked_users = chunked(active_users[:-extra_count], size)
+        for i, user in enumerate(extra_users):
+            chunked_users[i].append(user)
+        return chunked_users
+
+
+class User(_Base):
+    __tablename__ = 'users'
+
+    email = Column(String(100), primary_key=True)
+    name = Column(String(100))
+    _active = Column(Boolean)
+
+    def __repr__(self):
+        return f"<User(name='{self.name}', email='{self.email}', active='{self._active}')>"
