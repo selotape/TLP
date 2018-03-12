1b22b80d836919779cbf021ff4d66f7cf05b2387
Ron <ronvisbord@gmail.com>
2018-03-12 22:40:23 +0200

    minor refactor logging

--- TLP/__main__.py
+++ TLP/__main__.py
@@ -1,4 +1,4 @@
-from logging.config import fileConfig as loggingFileConfig
+from logging.config import fileConfig
 
 from TLP.google.oauth.google_auth import app
 
@@ -6,7 +6,7 @@ LOGGING_CONFIG = './logging_config.ini'
 
 if __name__ == '__main__':
     try:
-        loggingFileConfig(LOGGING_CONFIG)
+        fileConfig(LOGGING_CONFIG)
     except KeyError as e:
         print(f"Couldn't locate the {LOGGING_CONFIG} file")
     app.run(debug=True)
